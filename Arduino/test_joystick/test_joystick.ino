fa8d55b560193e117db3457f9638994e73531980
dexter258 <tom92mlynski@wp.pl>
2017-06-19 03:22:51 +0200

    no message

--- Arduino/test_joystick/test_joystick.ino
+++ Arduino/test_joystick/test_joystick.ino
@@ -2,17 +2,29 @@
 #include "Adafruit_MCP23017.h"
 
 Adafruit_MCP23017 mcp;
-#define lewo A
-#define prawo 
+#define lewo A0
+#define prawo 5
+#define gora 6
+#define dol 7
+#define strzal 8
+$define stycznik 4
+byte x;
+boolean Joystick[5];
+boolean start=false;
+boolean left=false;
+boolean right=false;
+boolean up=false;
+boolean down=false;
 void setup() {  
   mcp.begin();      // use default address 0
 for (int i=0;i<16;i++){
   mcp.pinMode(i, OUTPUT);
     mcp.digitalWrite(i,LOW);
 }
+pinMode(stycznik,INPUT);
 pinMode(lewo,INPUT);
 pinMode(prawo,INPUT);
-pinMode(gota,INPUT);
+pinMode(gora,INPUT);
 pinMode(dol,INPUT);
 pinMode(strzal,INPUT);
 
@@ -23,7 +35,53 @@ pinMode(strzal,INPUT);
 
 
 void loop() {
-//duupa 
-//tak adwadwad
+ if (digitalRead(stycznik)==HIGH){
+   start=true;
+ }
+ 
+ if (start==true){
+   Joystick[0]=false;
+   Joystick[1]=false;
+   Joystick[2]=false;
+   Joystick[3]=false;
+   Joystick[4]=false;
+   
+  if ((digitalRead(lewo)==LOW)&& (Left==true)){
+   left=false;
+  } 
+   if ((digitalRead(lewo)==HIGH)&& (Left==false)){ 
+     Joystick[0]=true;
+     Left=true;
+   }
+  x=random(0,3);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
 
 }
