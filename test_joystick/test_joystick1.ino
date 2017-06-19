db12f6d243ce0b79312aca1321fc0090573f53d4
dexter258 <tom92mlynski@wp.pl>
2017-06-19 02:08:15 +0200

    usuniecie kodu z loop

---
+++ test_joystick/test_joystick1.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include "Adafruit_MCP23017.h"
+
+Adafruit_MCP23017 mcp;
+#define lewo A
+#define prawo 
+void setup() {  
+  mcp.begin();      // use default address 0
+for (int i=0;i<16;i++){
+  mcp.pinMode(i, OUTPUT);
+    mcp.digitalWrite(i,LOW);
+}
+pinMode(lewo,INPUT);
+pinMode(prawo,INPUT);
+pinMode(gota,INPUT);
+pinMode(dol,INPUT);
+pinMode(strzal,INPUT);
+
+
+
+}
+
+
+
+void loop() {
+
+
+}
